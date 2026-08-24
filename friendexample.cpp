504f888c9a5de4b9ff9e0274e6c6041a3eede55e
Harshityadav9838 <harshityadavyadav9838@gmail.com>
2026-08-24 10:30:30 +0530

    eh

--- friendexample.cpp
+++ friendexample.cpp
@@ -53,25 +53,94 @@
 
 //friend function accessing public and private members 
 
+// #include <iostream>
+// using namespace std;
+// class employee {
+//     private:
+//     int salary;
+//     public:
+//     string name;
+//     employee(string n, int s){
+//         name = n;
+//         salary = s;
+//     }
+//     friend void display(employee e);
+// };
+// void display(employee e){
+//     cout << "Name: " << e.name << endl;
+//     cout << "Salary: " << e.salary << endl;
+// }
+// int main(){
+//     employee e1("John", 50000);
+//     display(e1);
+//     return 0;
+// }
+
+//friend function for adding two object 
+
+// #include <iostream>
+// using namespace std;
+// class number {
+//     private:
+//     int value;
+//     public:
+//     number(int v){
+//         value = v;
+//     }
+//     friend number add(number n1, number n2);
+//     void display(){
+//         cout << "Value: " << value << endl;
+//     }
+// };
+// number add(number n1, number n2){
+//     return number(n1.value + n2.value);
+// }
+// int main(){
+//     number n1(10);
+//     number n2(20);
+//     number n3 = add(n1, n2);
+//     n3.display();
+//     return 0;
+// }
+
 #include <iostream>
 using namespace std;
-class employee {
-    private:
-    int salary;
-    public:
-    string name;
-    employee(string n, int s){
-        name = n;
-        salary = s;
+
+class B;   // Forward declaration
+
+class A {
+private:
+    int x;
+
+public:
+    A() {
+        x = 46;
     }
-    friend void display(employee e);
+
+    friend void add(A, B);
 };
-void display(employee e){
-    cout << "Name: " << e.name << endl;
-    cout << "Salary: " << e.salary << endl;
+
+class B {
+private:
+    int y;
+
+public:
+    B() {
+        y = 79;
+    }
+
+    friend void add(A, B);
+};
+
+void add(A a, B b) {
+    cout << "Sum: " << a.x + b.y << endl;
 }
-int main(){
-    employee e1("John", 50000);
-    display(e1);
+
+int main() {
+    A a;
+    B b;
+
+    add(a, b);
+
     return 0;
 }
